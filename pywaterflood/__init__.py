50f4a050b3cbd957a20e76bb7223d5b119409ce9
Frank Male <frmale@utexas.edu>
2021-12-11 00:07:35 -0600

    fix: added __all__ declaration for __init__

--- pywaterflood/__init__.py
+++ pywaterflood/__init__.py
@@ -3,4 +3,9 @@ from importlib.metadata import version
 
 __version__ = version("pywaterflood")
 from .crm import CRM
-from .multiwellproductivity import calc_gains_homogeneous, translate_locations
+from .multiwellproductivity import (  # noqa: F401
+    calc_gains_homogeneous,
+    translate_locations,
+)
+
+__all__ = ["CRM", "calc_gains_homogeneous", "translate_location"]
