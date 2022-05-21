2f1121945f53028c5f73ea4c38195db898f2850d
Frank Male <frmale@utexas.edu>
2022-05-20 20:04:40 -0400

    :white_check_mark: tests: added automatic prediction file generation script

---
+++ testing/conftest.py
@@ -0,0 +1,3 @@
+from __future__ import annotations
+
+collect_ignore = ["regenerate_results.py"]
---
+++ testing/regenerate_results.py
@@ -0,0 +1,67 @@
+"""Regenerate results.
+
+This is for regenerating the results that are tested against during test_predict. Every once in
+a while it makes sense to regenerate it, but be careful.
+
+functions
+--------
+test_predict
+
+use
+----
+run `python testing/regenerate_results.py` from the project directory
+"""
+from __future__ import annotations
+
+import numpy as np
+
+from pywaterflood import CRM
+
+# from pywaterflood.crm import CrmCompensated, q_bhp
+
+data_dir = "testing/data/"
+
+
+def reservoir_simulation_data():
+    injection = np.genfromtxt(data_dir + "injection.csv", delimiter=",")
+    production = np.genfromtxt(data_dir + "production.csv", delimiter=",")
+    time = np.genfromtxt(data_dir + "time.csv", delimiter=",")
+    return injection, production, time
+
+
+def _trained_model(*args, **kwargs):
+    crm = CRM(*args, **kwargs)
+    crm.injection, crm.production, crm.time = reservoir_simulation_data()
+    crm.gains = np.genfromtxt(data_dir + "gains.csv", delimiter=",")
+    if crm.tau_selection == "per-pair":
+        crm.tau = np.genfromtxt(data_dir + "taus_per-pair.csv", delimiter=",")
+    else:
+        crm.tau = np.genfromtxt(data_dir + "taus.csv", delimiter=",")
+    crm.gains_producer = np.genfromtxt(data_dir + "gain_producer.csv", delimiter=",")
+    crm.tau_producer = np.genfromtxt(data_dir + "tau_producer.csv", delimiter=",")
+    return crm
+
+
+def test_predict(primary, tau_selection):
+    """Predictions for test cases."""
+    crm = _trained_model(primary=primary, tau_selection=tau_selection)
+
+    prediction = crm.predict()
+    return prediction
+
+
+if __name__ == "__main__":
+    print("Regenerating prediction files")
+    primary = (True, False)
+    tau_selection = ("per-pair", "per-producer")
+    for p in primary:
+        for t in tau_selection:
+            prediction = test_predict(p, t)
+            if p:
+                primary_str = "primary"
+            else:
+                primary_str = "noprimary"
+            np.savetxt(
+                f"{data_dir}prediction_{primary_str}_{t}.csv", prediction, delimiter=","
+            )
+    print("Finished")
