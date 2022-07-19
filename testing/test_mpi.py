daae085bdfabd22c5dc9842e951cbc55e00906b4
frank1010111 <frmale@utexas.edu>
2022-07-19 14:25:13 -0400

    :white-check-mark: further testing

--- testing/test_mpi.py
+++ testing/test_mpi.py
@@ -4,11 +4,37 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from pywaterflood.multiwellproductivity import (  # calc_gains_homogeneous,; translate_locations,
+from pywaterflood.multiwellproductivity import (
     calc_A_ij,
+    calc_gains_homogeneous,
+    calc_influence_matrix,
+    translate_locations,
 )
 
 raw_locs = pd.DataFrame()
+
+
+@pytest.fixture
+def locations():
+    "Well locations for an inverted 5-spot."
+    xy = [(1, 1), (20, 1), (1, 20), (20, 20), (10.5, 10.5)]
+    x = np.array([x for x, _ in xy]) + np.random.normal(0, 0.5, len(xy))
+    y = np.array([y for _, y in xy]) + np.random.normal(0, 0.5, len(xy))
+    locations = pd.DataFrame(
+        {
+            "x": x,
+            "y": y,
+            "Type": ["Producer"] * 4 + ["Injector"],
+        }
+    )
+    locations = translate_locations(locations, "x", "y", "Type")
+    return locations
+
+
+def test_translate(locations):
+    locations = translate_locations(locations, "X", "Y", "Type")
+    assert locations["X"].min() <= 1e-10
+    assert locations["Y"].min() <= 1e-10
 
 
 def test_Aij():
@@ -16,3 +42,22 @@ def test_Aij():
     m = 1 + np.arange(300)
     results = calc_A_ij(0.233351184, 0.36666667, 0.23333333, 0.36666667, 0.5, m)
     assert pytest.approx(10.6867, abs=0.01) == results
+
+
+def test_calc_gains(locations):
+    """See that calc_gains works."""
+    x_e = 34
+    y_e = 22
+    gains = calc_gains_homogeneous(locations, x_e, y_e)
+    assert np.all(gains <= 0), "Gains must be negative"
+    assert np.all(gains >= -1), "Gains can't be stronger than one"
+
+
+def test_influence_matrix(locations):
+    locations["X"] /= 20
+    locations["Y"] /= 20
+    matrix_conn = calc_influence_matrix(locations, y_D=0.7, matrix_type="conn", m_max=100)
+    assert not np.isnan(matrix_conn.values).flatten().any()
+
+    matrix_prod = calc_influence_matrix(locations, y_D=0.7, matrix_type="prod", m_max=100)
+    assert not np.isnan(matrix_prod.values).flatten().any()
