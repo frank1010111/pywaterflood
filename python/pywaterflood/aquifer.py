7a7aafde4b33f9909d4f6cdaac7f9f2eb9b2effa
Frank Male <frmale@utexas.edu>
2024-07-13 12:06:20 -0400

    :sparkles: aquifer cumulative production that works with the Petrowiki example

--- python/pywaterflood/aquifer.py
+++ python/pywaterflood/aquifer.py
@@ -29,8 +29,6 @@ from numpy.typing import NDArray
 from scipy.optimize import root_scalar
 from scipy.special import j0, j1, y0, y1
 
-from pywaterflood import _core
-
 
 def effective_reservoir_radius(
     reservoir_pore_volume: float, porosity: float, thickness: float, flow_solid_angle: float = 360
@@ -62,6 +60,43 @@ def effective_reservoir_radius(
     return math.sqrt(
         5.6146 * reservoir_pore_volume / (math.pi * porosity * thickness * flow_solid_angle / 360)
     )
+
+
+def aquifer_production(
+    delta_pressure: NDArray,
+    t_d: NDArray,
+    r_ed: float,
+    method: Literal["marsal-walsh", "klins"] = "klins",
+    aquifer_constant: float = 1.0,
+) -> NDArray:
+    r"""Calculate cumulative aquifer production.
+
+    Parameters
+    ----------
+    delta_pressure : NDArray
+        change in reservoir pressure
+    t_d : NDArray
+        dimensionless time
+    r_ed : float
+        dimensionless radius, :math:`r_a/r_o`
+    method : Literal str
+        choose 'marsal-walsh' or 'klins' approximation
+    aquifer_constant : float
+        aquifer constant in RB/psi
+
+        :math:`1.119 hfr^2_o \phi c_t`
+
+    Returns
+    -------
+    NDArray
+        Cumulative production from the aquifer
+    """
+    n_t = len(t_d)
+    w_ek = np.zeros(n_t)
+    for k in range(1, n_t):
+        t_dk = t_d[k] - np.array(t_d)[:k]
+        w_ek[k] = sum(water_dimensionless(t_dk, r_ed, method) * delta_pressure[1 : k + 1])
+    return aquifer_constant * w_ek
 
 
 def water_dimensionless(
@@ -169,72 +204,18 @@ def water_dimensionless_infinite(
     return water_influx
 
 
-def aquifer_production(delta_pressure: NDArray, w_d: NDArray, aquifer_constant: float) -> NDArray:
-    r"""Calculate cumulative aquifer production.
-
-    Parameters
-    ----------
-    delta_pressure : NDArray
-        change in reservoir pressure
-    w_d : NDArray
-        dimensionless water influx
-    aquifer_constant : float
-        aquifer constant in RB/psi
-
-        :math:`1.119 hfr^2_o \phi c_t`
-
-    Returns
-    -------
-    NDArray
-        Cumulative production from the aquifer
-    """
-    w_ek = _core.w_ek(w_d, delta_pressure)
-    return aquifer_constant * w_ek
-
-
-def bessel_roots(r_ed: float, n_max: int) -> NDArray[np.float64]:
+def get_bessel_roots(r_ed: float, n_max: int, root_choice="beta") -> NDArray[np.float64]:
     r"""Find roots of Bessel function in Klins.
 
-    Comes from Klins, 1988, eq 9
+    Comes from Klins, 1988, eq 9,
 
     .. math::
 
         J_1(\beta_n r_{eD}) Y_1(\beta_n) - J_1(\beta_n) Y_1(\beta_n r_{eD})
 
-    Parameters
-    ----------
-    r_ed : float
-        dimensionless reservoir radius, :math:`r_o/r_a`
-    n_max : int
-        number of roots to find
-
-    Returns
-    -------
-    numpy array:
-        roots of the function
-    """
-
-    def root_func(beta):
-        return j1(beta * r_ed) * y1(beta) - j1(beta) * y1(beta * r_ed)
-
-    sample_betas = np.linspace(1e-9, 8 * n_max / r_ed, n_max * 400)
-    zero_crossings = np.array([])
-    while len(zero_crossings) < n_max:
-        sample_betas *= 2
-        zero_crossings = np.where(np.diff(np.sign(root_func(sample_betas))))[0]
-    zero_crossings = zero_crossings[:n_max]
-
-    roots = [root_scalar(root_func, x0=sample_betas[zc]).root for zc in zero_crossings]
-    return np.asarray(roots)
-
-
-def get_alphas(r_ed, n_max):
-    r"""Find roots of Bessel function in Klins.
-
-    Comes from Klins, 1988, eq 16
+    and eq 16
 
     .. math::
-
         J_1(\alpha_n r_{eD}) Y_0(\alpha_n) - Y_1(\alpha_n) J_0(\alpha_n r_{eD})
 
     Parameters
@@ -243,6 +224,10 @@ def get_alphas(r_ed, n_max):
         dimensionless reservoir radius, :math:`r_o/r_a`
     n_max : int
         number of roots to find
+    root_choice: str
+        which roots to get.
+
+        for eq 9, `beta`, for eq 16, `alpha`
 
     Returns
     -------
@@ -250,17 +235,28 @@ def get_alphas(r_ed, n_max):
         roots of the function
     """
 
-    def root_func(alpha):
+    def root_func_beta(beta):
+        return j1(beta * r_ed) * y1(beta) - j1(beta) * y1(beta * r_ed)
+
+    def root_func_alpha(alpha):
         return j1(alpha * r_ed) * y0(alpha) - y1(alpha * r_ed) * j0(alpha)
 
-    sample_alphas = np.linspace(1e-9, 2 * n_max / r_ed, n_max * 400)
+    if root_choice == "beta":
+        root_func = root_func_beta
+    elif root_choice == "alpha":
+        root_func = root_func_alpha
+    else:
+        msg = f"root choice must be either alpha or beta, but {root_choice}"
+        raise ValueError(msg)
+
+    sample_roots = np.linspace(1e-9, 8 * n_max / r_ed, n_max * 400)
     zero_crossings = np.array([])
     while len(zero_crossings) < n_max:
-        sample_alphas *= 2
-        zero_crossings = np.where(np.diff(np.sign(root_func(sample_alphas))))[0]
+        sample_roots *= 2
+        zero_crossings = np.where(np.diff(np.sign(root_func(sample_roots))))[0]
     zero_crossings = zero_crossings[:n_max]
 
-    roots = [root_scalar(root_func, x0=sample_alphas[zc]).root for zc in zero_crossings]
+    roots = [root_scalar(root_func, x0=sample_roots[zc]).root for zc in zero_crossings]
     return np.asarray(roots)
 
 
@@ -288,7 +284,7 @@ def klins_pressure_dimensionless(
         4 * (r_ed**2 - 1) ** 2
     )
     third_term = 0.0
-    betas = bessel_roots(r_ed, max_terms)
+    betas = get_bessel_roots(r_ed, max_terms)
     for n in range(max_terms):
         third_term += (
             2
@@ -320,7 +316,7 @@ def klins_water_dimensionless_finite(
     """
     first_term = 0.5 * (r_ed**2 - 1)
     second_term = 0
-    alphas = get_alphas(r_ed, max_terms)
+    alphas = get_bessel_roots(r_ed, max_terms)
     for n in range(1, max_terms):
         second_term += (
             -2
